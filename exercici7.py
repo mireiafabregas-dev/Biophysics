2d7a31fa7525d9a3318da23ebeea890d5425255c
mireiafabregas-dev <mireia.fabregas@estudiantat.upc.edu>
2026-09-15 09:27:32 +0200

    Update exercici7.py

--- exercici7.py
+++ exercici7.py
@@ -43,3 +43,46 @@ for at10 in res10.get_atoms():
     print(at10, distance)
 
 # example: python3 exercici7.py 1UBQ --res1 10 --res2 20 --chain1 A --chain2 A
+
+""" 
+output example
+
+Residue 10 is GLY
+Residue 20 is SER
+
+Atom1 Atom2 dist1 dist2
+-------------------------
+<Atom N> <Atom N> 26.841194 26.841194
+<Atom N> <Atom CA> 27.705828 27.705826
+<Atom N> <Atom C> 27.01152 27.011518
+<Atom N> <Atom O> 27.704405 27.704405
+<Atom N> <Atom CB> 28.923367 28.923365
+<Atom N> <Atom OG> 28.661741 28.661741
+<Atom CA> <Atom N> 26.76021 26.76021
+<Atom CA> <Atom CA> 27.68607 27.68607
+<Atom CA> <Atom C> 27.0538 27.0538
+<Atom CA> <Atom O> 27.799332 27.799332
+<Atom CA> <Atom CB> 28.883638 28.883638
+<Atom CA> <Atom OG> 28.57509 28.57509
+<Atom C> <Atom N> 26.097872 26.097872
+<Atom C> <Atom CA> 27.042477 27.042479
+<Atom C> <Atom C> 26.412207 26.412207
+<Atom C> <Atom O> 27.175257 27.175257
+<Atom C> <Atom CB> 28.193848 28.19385
+<Atom C> <Atom OG> 27.822264 27.822264
+<Atom O> <Atom N> 26.35525 26.35525
+<Atom O> <Atom CA> 27.34624 27.34624
+<Atom O> <Atom C> 26.76351 26.76351
+<Atom O> <Atom O> 27.56535 27.56535
+<Atom O> <Atom CB> 28.469149 28.469149
+<Atom O> <Atom OG> 28.049194 28.049194
+
+Distance of res10 to [10 10 10]
+
+<Atom N> 39.46418688865108
+<Atom CA> 39.183283863832415
+<Atom C> 38.96855544105957
+<Atom O> 39.107196877658936
+
+
+"""
